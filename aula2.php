c8c3028e9c89cc016bef21e720a810f43e7739f8
SilvPedro11 <pedoca.360@gmail.com>
2022-08-10 20:24:33 -0300

    Atualização

---
+++ aula2.php
@@ -0,0 +1,21 @@
+<?php  
+
+	$valor = 12;
+	$valor = "Etecia";
+	$valor = true;
+	$valor = 25.5;
+
+	$valor = "Olá Mundo!!!.";
+
+	echo 'Eu escrevi algo para ver <br>';
+	print('Eu escrevi algo para ver <br>');
+	print date('Y-m-d');
+?>
+
+<?php  
+
+	$a = 10;
+
+	echo $a;
+
+?>
