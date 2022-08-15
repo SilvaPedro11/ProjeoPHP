ad4c49c2ac6383818cd9962127ccd25cd2554c54
SilvaPedro <pedoca.360@gmail.com>
2022-08-15 20:26:13 -0300

    atualização

---
+++ aula3.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<?php  
+	#Testando comentário de Hash Mark
+	#Etec Irmã Agostina	
+?>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<?php 
+
+	/* Este é um comentário
+	de multiplas linhas
+	*/
+	echo "Segundo exemplo em PHP";
+	$data = "15 de agosto de 2022";
+
+	//Este é um comentário de apenas uma linha
+	 ?>
+	 <p>Este parágrafo está sendo interpretado como HTML</p>
+	 Arquivo criado em <?php echo $data ?>
+</body>
+</html>
+
+
