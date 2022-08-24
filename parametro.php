964e31cfb1edbe0888307f2eaed10ac63a528219
SilvaPedro11 <pedoca.360@gmail.com>
2022-08-24 20:21:29 -0300

    mais exemplos

---
+++ parametro.php
@@ -0,0 +1,14 @@
+<?php 
+
+$ano = 2020; //variável local script
+
+//implementação de uma função
+function exibir($parametro){ //variável como parâmetro
+	$parametro = $parametro + 4;
+	return $parametro;
+
+}
+
+echo "Estamos em " .$ano. " e daqui a 4 anos estaremos em ".exibir($ano);
+
+ ?>
---
+++ superglobal.php
@@ -0,0 +1,6 @@
+<?php 
+
+	echo "<br/> Olá Seja Bem Vindo".$_SERVER['REMOTE_ADDR']	."<br/>";
+	echo "Você está utilizando navegador: ".$_SERVER['HTTP_USER_AGENT'];
+
+ ?>
